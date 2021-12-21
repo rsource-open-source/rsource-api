7fab0c15033e31ebfe0eb294a71af475cc392fb6
insyri <60794909+insyri@users.noreply.github.com>
2021-12-21 00:33:46 -0500

    Update main.go

--- src/main.go
+++ src/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("The rsource API.")
+    fmt.Println("The rsource API allows developers to submit database queries to the rsource records player database.")
 }
