5f289c02d345f2c282bdc2c23a0119cc595276cc
insyri <60794909+insyri@users.noreply.github.com>
2022-02-20 01:36:12 -0500

    init discord, #15

---
+++ discord/client.go
@@ -0,0 +1,18 @@
+package DiscordClient
+
+// perms int: 101376
+
+import (
+	"github.com/andersfylling/disgord"
+)
+
+const (
+	UploadChannelID = disgord.Snowflake(944410841812459520)
+)
+
+// func main() {
+// 	client := disgord.New(disgord.Config{
+// 		BotToken: os.Getenv("DISCORD_TOKEN"),
+// 	})
+
+// }
